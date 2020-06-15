a10e3fc077c4d9d167b6e11f63481770204501e8
AnantkrJain <anantkr.developer@gmail.com>
2020-06-15 13:26:52 +0530

    Basic Arithmetic Calculation

---
+++ Project3/src/Arithmetic/Maths.java
@@ -0,0 +1,19 @@
+package Arithmetic;
+
+public class Maths {
+	public static void main(String[] args) {
+		
+		int num1=5, num2=7, Result1, Result2, Result4;
+		double Result3;
+		
+		Result1 = num2 + num1;
+		Result2 = num2 * num1;
+		Result3 = num2 / num1;
+		Result4 = num2 - num1;
+		
+		System.out.println("Addition of result is " + Result1);
+		System.out.println("Multiplication of result is " + Result2);
+		System.out.println("Division of result is " + Result3);
+		System.out.println("Substraction of result is " + Result4);
+	}
+}
